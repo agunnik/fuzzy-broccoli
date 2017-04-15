e1d4fdb48b4b9eaf781d1444fb599c6de2b83dea
agunnik <agunnik*@gmail.com>
2017-04-15 13:53:38 +0530

    Changes made to few more bitflags. Now completely in raw mode.

--- fuzzy-broccoli.c
+++ fuzzy-broccoli.c
@@ -1,25 +1,53 @@
 #include<unistd.h>
 #include<termios.h>
 #include<stdlib.h>
+#include<errno.h>
 #include<ctype.h>
 #include<stdio.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
 struct termios org_termios;
 
+void die(const char *s){
+	perror(s);
+	exit(1);
+}
+
+//restores canonical mode.
 void disableRawMode(){
 	//TCSAFLUSH flushes any input before setting the bitflags.
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &org_termios);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &org_termios) == -1) 
+		die("tcsetattr");
 }
 
 void enableRawMode(){
-	tcgetattr(STDIN_FILENO, &org_termios);
+	if(tcgetattr(STDIN_FILENO, &org_termios) == -1)
+		die("tcgetattr");
 	struct termios raw = org_termios;
 	
-	//registering the disableRawMode() function to run when the program exits.
+	//registering the disableRawMode() function to run when the program exits. atexit() is from stdlib.h.
 	atexit(disableRawMode);
+
 	//ICANON bitflag sets the input in canonical mode.
-	raw.c_lflag &= ~(ECHO | ICANON);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	//ISIG enables sigs INTR, QUIT & SUSP
+	//ECHO enables echoing.
+	//IEXTEN enables Ctrl-V and (only on macOS) Ctrl-O.
+
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+	//IXON is an input flag. It disables input control flow. Ctrl-s and Ctrl-Q can now be detected.
+	//fixes Ctrl-m with ICRNL.
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	//disabling output formatting.
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+
+	//setting timeout for read()
+	raw.c_cc[VMIN] = 0;
+	//raw.c_cc[VTIME] = 1;
+
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		die("tcsetattr");
 }
 
 int main(){
@@ -28,12 +56,19 @@ int main(){
 	//read and STDIN_FILENO is from unistd.h
 	//read() reads one character from stdin and stores it to c.
 	//read() reads until it reaches the EOF. This is signalled ny ^-D.
-	while (read(STDIN_FILENO, &c, 1) == 1 && c!='q'){
+	while (1){
+		c ='\0';
+		if(read(STDIN_FILENO, &c, 1) == -1 && errno == EAGAIN)
+			die("read");
+		if(c == CTRL_KEY('q')){
+			printf("EXIT\r\n");
+			break;
+		}
 		if(iscntrl(c)){
-			printf("%d\n",c);
+			printf("%d\r\n",c);
 		}
 		else{
-			printf("%d (%c)\n",c,c);
+			printf("%d (%c)\r\n",c,c);
 		}
 	}
 
